d2babd38dc424d5a93e05167e622efc8c5e1869b
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-05-16 21:36:03 +0530

    Create and style add-book modal

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const bodyEle = document.querySelector("body");
     const addBookBtn = document.querySelector(".add-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
     const themeBtn = document.querySelector(".theme-btn");
     const addBookModal = document.querySelector(".add-book-modal");
-    const icons = Array.from(document.querySelectorAll(".icon"));
     addBookBtn.addEventListener("click", () => {
         addBookModal.showModal();
     })
 
+    cancelBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        addBookModal.close();
+    })
+
     themeBtn.addEventListener("click", () => {
         bodyEle.classList.toggle("dark-mode");
-        // icons.forEach(icon => icon.classList.toggle("invert"));
     })
 })
